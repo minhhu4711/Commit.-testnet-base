c6e35e01de94ab4aaa3a1dd96035f31a5687e047
minhhu4711 <ngoclanvnhk9@gmail.com>
2026-03-09 11:43:02 +0700

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -14,3 +14,13 @@ def calculate_bmi(weight, height):
     # Áp dụng công thức: BMI = Cân nặng / (Chiều cao bình phương)
     # Trong Python, toán tử ** dùng để tính lũy thừa (ví dụ: height ** 2 là height bình phương)
     bmi = weight / (height ** 2)
+# 2. Phân loại tình trạng cơ thể dựa trên kết quả BMI theo tiêu chuẩn chung
+    if bmi < 18.5:
+        return "Gầy"
+    elif bmi < 24.9:
+        return "Bình thường"
+    elif bmi < 29.9:
+        return "Thừa cân"
+    else:
+        # Nếu BMI từ 29.9 trở lên
+        return "Béo phì"
