a7c6143fd37cfec23fa0bd03b6c3d79e13e575a9
minhhu4711 <ngoclanvnhk9@gmail.com>
2026-03-10 19:30:39 +0700

    Update theme_toggle.js

--- theme_toggle.js
+++ theme_toggle.js
@@ -9,3 +9,5 @@ btn.addEventListener("click", function() {
 });
 // 1. Chọn phần tử nút bấm có class là "btn-toggle" từ HTML
 const btn = document.querySelector(".btn-toggle");
+// 2. Lắng nghe sự kiện "click" trên nút bấm đó
+btn.addEventListener("click", function() {
