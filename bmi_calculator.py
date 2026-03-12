6ea0e1f32518338c1612bb3738d9873191666319
minhhu4711 <ngoclanvnhk9@gmail.com>
2026-03-12 08:58:22 +0700

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -27,7 +27,6 @@ def calculate_bmi(weight, height):
 # 3. Phần nhập liệu từ người dùng
 # Sử dụng float() để cho phép nhập số thập phân (ví dụ: 1.75 hoặc 65.5)
 # input() sẽ nhận dữ liệu từ bàn phím dưới dạng chuỗi, nên cần ép kiểu về float
-w = float(input("Nhập cân nặng (kg): "))
-h = float(input("Nhập chiều cao (m): "))
+
 
 
